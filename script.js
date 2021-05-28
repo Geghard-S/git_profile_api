014ccd53fb55384d1d16710e14d15398b0eaeba0
Geghard-S <geghard@gmail.com>
2021-05-28 16:26:27 -0700

    second commit

--- script.js
+++ script.js
@@ -7,9 +7,25 @@ const search = document.getElementById('search')
 async function getUser(username) {
     try {
         const { data } = await axios (APIURL + username)
+
         createUserCard(data)
+        getRepos(username)
     } catch (err) {
-        console.log(err)
+        if (err.response.status == 404){
+            createErrorCard('No such profile.')
+        }
+    }
+}
+
+async function getRepos(username){
+    try {
+        const { data } = await axios (APIURL + username + '/repos?sort=created')
+
+        addReposToCard(data)
+    } catch (err) {
+        if (err.response.status == 404){
+            createErrorCard('Problem with repos.')
+        }
     }
 }
 
@@ -20,23 +36,47 @@ function createUserCard (user) {
                 <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
             </div>
             <div class="user-info">
-            <h2>${user.name}</h2>
-            <p>${user.bio}</p>
-
+                <h2>${user.name}</h2>
+                <p>${user.bio}</p>
                 <ul>
                     <li>${user.followers} <strong>Followers</strong></li>
                     <li>${user.following} <strong>Following</strong></li>
                     <li>${user.public_repos} <strong>Repos</strong></li>
                 </ul>
 
-                <div id="repos">
-                </div>
+
+                <div id="repos"></div>
             </div>
         </div>
-       
     `
     main.innerHTML = cardHTML
 
+}
+
+function createErrorCard(msg) {
+    const cardHTML = `
+        <div class="card">
+            <h1>${msg}</h1>
+        </div>
+    `
+
+    main.innerHTML = cardHTML
+}
+
+function addReposToCard(repos) {
+    const reposEl = document.getElementById('repos')
+
+    repos
+        .slice(0, 25)
+        .forEach(repo => {
+            const repoEl = document.createElement('a')
+            repoEl.classList.add('repo')
+            repoEl.href= repo.html_url
+            repoEl.target = '_blank'
+            repoEl.innerText = repo.name
+
+            reposEl.appendChild(repoEl)
+        })
 }
 
 form.addEventListener('submit', (e) => {
